74a8873885c1d052eb268d3a1acdc2c9b39dc978
alireza dodangeh <ardsoft_2010@yahoo.com>
2022-06-14 21:54:18 +0430

    Add project files.

---
+++ Mahak.cs
@@ -0,0 +1,72 @@
+﻿using NewDLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TarazoMahak
+{
+    /// <summary>
+    /// کلاس اتصال به باسکول MDS13000
+    /// از نوع MDS
+    /// </summary>
+    public class Mahak
+    {
+        public int _ComPort { get; set; }
+
+        public NewMahakScale mahakScale { get; set; }
+
+        public MahakScale.MahakScale Scale { get; set; }
+
+        /// <summary>
+        /// پیش فرض با Com1 وصل میشه
+        /// </summary>
+        /// <param name="ComPort"></param>
+        public Mahak(int ComPort = 1)
+        {
+            _ComPort = ComPort;
+
+            mahakScale = new NewMahakScale(false, "", 0, "COM" + ComPort, 9600, true, true, true);
+
+            Scale = mahakScale.mahakScales[_ComPort];
+        }
+
+        /// <summary>
+        /// اتصال برقرار است؟
+        /// </summary>
+        /// <returns></returns>
+        public async Task<bool> Connected()
+        {
+            //اگه سه تاشم ترو باشه یعنی ترازو وصل هستش
+            return
+            Scale.isActive &&
+            Scale.isAvailable &&
+            Scale.isPortOpen;
+        }
+
+        /// <summary>
+        /// خوندن وزن ترازو
+        /// </summary>
+        /// <returns></returns>
+        public async Task<long> GetWeight()
+        {
+            if (Scale.ReadWeight())
+            {
+                //var Answers = Scale.Answer;
+                // دریافت عدد وزن = 
+
+                //return Answers[0];
+
+                //دریافت عدد وزن با روش دیگر 
+                return Scale.Weight;
+            }
+            else
+            {
+                //دستورد اجرا نشد
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TarazoMahak
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Mahak Mahak;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {//تنظیمات اولیه
+            try
+            {
+                Mahak = new Mahak(int.Parse(txtCom.Text));
+            }
+            catch (Exception ex)
+            {
+                txtLog.Text += ex.Message;
+                txtLog.Text += "\n\r";
+            }
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {//تست اتصال
+
+            try
+            {
+                txtLog.Text += "Connected: " + await Mahak.Connected();
+                txtLog.Text += "\n\r";
+            }
+            catch (Exception ex)
+            {
+                txtLog.Text += ex.Message;
+                txtLog.Text += "\n\r";
+            }
+
+            try
+            {
+                txtLog.Text += "isActive: " + Mahak.Scale.isActive;
+                txtLog.Text += "\n\r";
+            }
+            catch (Exception ex)
+            {
+                txtLog.Text += ex.Message;
+                txtLog.Text += "\n\r";
+            }
+
+            try
+            {
+                txtLog.Text += "isAvailable: " + Mahak.Scale.isAvailable;
+                txtLog.Text += "\n\r";
+            }
+            catch (Exception ex)
+            {
+                txtLog.Text += ex.Message;
+                txtLog.Text += "\n\r";
+            }
+
+            try
+            {
+                txtLog.Text += "isPortOpen: " + Mahak.Scale.isPortOpen;
+                txtLog.Text += "\n\r";
+            }
+            catch (Exception ex)
+            {
+                txtLog.Text += ex.Message;
+                txtLog.Text += "\n\r";
+            }
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {//خوندن وزن
+            try
+            {
+                txtLog.Text += "GetWeight: " + await Mahak.GetWeight();
+                txtLog.Text += "\n\r";
+            }
+            catch (Exception ex)
+            {
+                txtLog.Text += ex.Message;
+                txtLog.Text += "\n\r";
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string json = JsonConvert.SerializeObject(Mahak.Scale);
+
+                txtLog.Text += "json: " + json;
+                txtLog.Text += "\n\r";
+            }
+            catch (Exception ex)
+            {
+                txtLog.Text += ex.Message;
+                txtLog.Text += "\n\r";
+            }
+        }
+    }
+}
